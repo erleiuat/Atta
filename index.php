f4e0534e6caccfe77c78db54fdcd899d5a37005f
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-04-27 09:53:34 +0200

    BUGFIX

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
         </div>
 
         <!-- Bootstrap core JavaScript -->
-        <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js" integrity="sha384-lZmvU/TzxoIQIOD9yQDEpvxp6wEU32Fy0ckUgOH4EIlMOCdR823rg4+3gWRwnX1M" crossorigin="anonymous"></script>
 
         <!-- Plugin JavaScript -->
