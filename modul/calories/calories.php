535ec837747360a44cad5f60a56fbdd578d07b36
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-05-30 19:43:48 +0200

    Even more fckng improovments for fcks sake

--- modul/calories/calories.php
+++ modul/calories/calories.php
@@ -14,56 +14,76 @@
     <div class="my-auto">
 
         <h2 class="mb-0">Calories</h2>
-        <div class="col-lg-12">
-            <div class="row table-responsive">
+        <div class="col-lg-12" style="min-width: 100%;">
+            <div class="row">
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col">Date | Time</th>
+                                <th scope="col">Amount</th>
+                                <th scope="col">Calories (p.p.)</th>
+                                <th scope="col">Calories (total)</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col">Date | Time</th>
-                            <th scope="col">Amount</th>
-                            <th scope="col">Calories (p.p.)</th>
-                            <th scope="col">Calories (total)</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                            <?php
 
-                        <?php
+                                $sql = "SELECT * FROM `tb_user_cal` WHERE tb_user_ID = $session_userid ORDER BY `tb_user_cal`.`entryDate`  DESC";
 
-                            $sql = "SELECT * FROM `tb_user_cal` WHERE tb_user_ID = $session_userid ORDER BY `tb_user_cal`.`entryDate`  DESC";
+                                $res = $mysqli->query($sql);
+                                if (isset($res) && $res->num_rows > 0) {
 
-                            $res = $mysqli->query($sql);
-                            if (isset($res) && $res->num_rows > 0) {
+                                    $dateVar = "";
+                                    $dates = 0;
+                                    $tmpEntry = "";
+                                    $tmpCalSum = 0;
 
-                                $dateVar = "";
-                                $dates = 0;
-                                $tmpEntry = "";
-                                $tmpCalSum = 0;
+                                    while($row = $res->fetch_assoc()){
 
-                                while($row = $res->fetch_assoc()){
+                                        if(substr($dateVar, 0, -9) != substr($row['entryDate'], 0, -9)){
 
-                                    if(substr($dateVar, 0, -9) != substr($row['entryDate'], 0, -9)){
+                                            if($dateVar != ""){
 
-                                        if($dateVar != ""){
-                                            echo '
-                                            <tr style="cursor:pointer;" onclick="openDayCal('.$dates.');">
-                                                <th colspan="2" scope="row">'.substr($dateVar, 0, -9).'</th>
-                                                <td></td>
-                                                <td></td>
-                                                <td colspan="2">'.$tmpCalSum.'</td>
-                                            </tr>
-                                            '.$tmpEntry;
+                                                $date = substr($dateVar, 0, -9);
+                                                $weekday = date('l', strtotime($date)); // note: first arg to date() is lower-case L
 
-                                            $dates++;
-                                            $tmpEntry = "";
-                                            $tmpCalSum = 0;
-                                            $dateVar = $row['entryDate'];
+                                                echo '
+                                                <tr style="cursor:pointer;" onclick="openDayCal('.$dates.');">
+                                                    <th colspan="2" scope="row">'. $weekday .', '.$date.'</th>
+                                                    <td></td>
+                                                    <td></td>
+                                                    <td colspan="2">'.$tmpCalSum.'</td>
+                                                </tr>
+                                                '.$tmpEntry;
+
+                                                $dates++;
+                                                $tmpEntry = "";
+                                                $tmpCalSum = 0;
+                                                $dateVar = $row['entryDate'];
+
+                                            } else {
+
+                                                $dateVar = $row['entryDate'];
+                                                $tmpCalSum += $row['calories']*$row['amount'];
+                                                $tmpEntry .= '
+                                                <tr class="removableEntry" style="display:none;" id="'.$row['ID'].'" dayID="'.$dates.'">
+                                                    <th scope="row">'.$row['title'].'</th>
+                                                    <td>'.$row['entryDate'].'</td>
+                                                    <td>'.$row['amount'].'</td>
+                                                    <td>'.$row['calories'].'</td>
+                                                    <td>'.$row['calories']*$row['amount'].'</td>
+                                                    <td class="deleteCalEntry" id="'.$row['ID'].'"><i style="cursor:pointer;" class="far fa-trash-alt"></i></td>
+                                                </tr>
+                                                ';
+
+                                            }
 
                                         } else {
 
-                                            $dateVar = $row['entryDate'];
                                             $tmpCalSum += $row['calories']*$row['amount'];
                                             $tmpEntry .= '
                                             <tr class="removableEntry" style="display:none;" id="'.$row['ID'].'" dayID="'.$dates.'">
@@ -78,40 +98,27 @@
 
                                         }
 
-                                    } else {
-
-                                        $tmpCalSum += $row['calories']*$row['amount'];
-                                        $tmpEntry .= '
-                                        <tr class="removableEntry" style="display:none;" id="'.$row['ID'].'" dayID="'.$dates.'">
-                                            <th scope="row">'.$row['title'].'</th>
-                                            <td>'.$row['entryDate'].'</td>
-                                            <td>'.$row['amount'].'</td>
-                                            <td>'.$row['calories'].'</td>
-                                            <td>'.$row['calories']*$row['amount'].'</td>
-                                            <td class="deleteCalEntry" id="'.$row['ID'].'"><i style="cursor:pointer;" class="far fa-trash-alt"></i></td>
-                                        </tr>
-                                        ';
-
                                     }
+
+                                    $date = substr($dateVar, 0, -9);
+                                    $weekday = date('l', strtotime($date)); // note: first arg to date() is lower-case L
+
+                                    echo '
+                                    <tr style="cursor:pointer;" onclick="openDayCal('.$dates.');">
+                                        <th colspan="2" scope="row">'. $weekday .', '.$date.'</th>
+                                        <td></td>
+                                        <td></td>
+                                        <td colspan="2">'.$tmpCalSum.'</td>
+                                    </tr>
+                                    '.$tmpEntry;
 
                                 }
 
-                                echo '
-                                <tr style="cursor:pointer;" onclick="openDayCal('.$dates.');">
-                                    <th colspan="2" scope="row">'.substr($dateVar, 0, -9).'</th>
-                                    <td></td>
-                                    <td></td>
-                                    <td colspan="2">'.$tmpCalSum.'</td>
-                                </tr>
-                                '.$tmpEntry;
+                            ?>
 
-                            }
-
-                        ?>
-
-                    </tbody>
-                </table>
-
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
