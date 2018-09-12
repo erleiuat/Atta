4604b3c71430adbd168388fb2a95cf1cc4090e97
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2018-09-12 15:43:33 +0200

    External DB Credentials

--- include/database.php
+++ include/database.php
@@ -6,10 +6,7 @@
         $_db_username = getenv('APP_DB_USER');
         $_db_passwort = getenv('APP_DB_PASS');
     } else {
-        $_db_database = "db_atta";
-        $_db_host = "localhost";
-        $_db_username = "root";
-        $_db_passwort = "";
+        include('include/database/dbCredentials.php');
     }
 
     $mysqli = mysqli_connect($_db_host, $_db_username, $_db_passwort, $_db_database);
---
+++ include/database/dbCredentials.php
@@ -0,0 +1,8 @@
+<?php
+
+    $_db_database = "db_atta";
+    $_db_host = "localhost";
+    $_db_username = "root";
+    $_db_passwort = "";
+
+?>
